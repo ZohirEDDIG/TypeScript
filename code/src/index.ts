97a775488277d0ad6c20ac82d275842407ce53cd
ZohirEDDIG <zohireddig.dev@gmail.com>
2026-03-04 13:21:20 +0000

    12 TS - Functions

--- code/src/index.ts
+++ code/src/index.ts
@@ -1,7 +1,50 @@
-function printMyResponseStatus(status: string | number) {
-    console.log(`My status is '${status}'`);
-    // console.log(`My status is '${status.toUpperCase()}'`); // error: Property 'toUpperCase' does not exist on type 'string | number'. Property 'toUpperCase' does not exist on type 'number'
+function getTime(): number {
+    return new Date().getTime();
 }
 
-console.log(printMyResponseStatus(200));
-console.log(printMyResponseStatus('success'));
+console.log(getTime());
+
+function printHello(): void {
+    console.log('Hello!');
+}
+
+printHello();
+
+function multiply(a: number, b: number) {
+    return a * b;
+}
+
+console.log(multiply(5, 2));
+
+// function add(a: number, b: number, c?: number) {
+//     return a + b + (c || 0);
+// }
+
+// console.log(add(1, 2, 3));
+// console.log(add(1, 2));
+
+function pow(value: number, exponent: number = 10) {
+    return value ** exponent;
+}
+
+console.log(pow(2, 2));
+console.log(pow(2));
+
+function divide({ dividend, divisor }: { dividend: number; divisor: number }) {
+    return dividend / divisor;
+}
+
+console.log(divide({ dividend: 10, divisor: 2 }));
+
+function add(a: number, b: number, ...rest: number[]) {
+    return a + b + rest.reduce((p, c) => p + c, 0);
+}
+
+console.log(add(10, 10, 10, 10, 10));
+
+
+type Negate = (value: number) => number;
+
+const negateFunction: Negate = (value) => value * -1;
+
+console.log(negateFunction(10))
